95c3a0f199f12ceffa43322d836e7f08e54a7c2c
IvanFerroli <ivanilson.ferreira.mec@gmail.com>
2023-09-03 12:26:44 -0300

    feat: Listagem de categorias e de transações

--- src/rotas.js
+++ src/rotas.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { cadastrarUsuario, login, detalharUsuario } = require('./controladores/usuarios');
 const verificarLogin = require('./intermediarios/verificarLogin');
+const { listarCategorias } = require('./controladores/categorias');
+const { listarTransacoes } = require('./controladores/transacoes');
 
 const rotas = express();
 
@@ -10,5 +12,7 @@ rotas.post('/login', login);
 rotas.use(verificarLogin);
 
 rotas.get('/usuario', detalharUsuario)
+rotas.get('/categoria', listarCategorias)
+rotas.get('/transacao', listarTransacoes)
 
 module.exports = rotas;
