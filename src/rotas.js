b6f584c630be0155cf9ff285e02217b1126271c0
IvanFerroli <ivanilson.ferreira.mec@gmail.com>
2023-09-03 20:59:37 -0300

    feat: buscar transações por id e editar transações

--- src/rotas.js
+++ src/rotas.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { cadastrarUsuario, login, detalharUsuario } = require('./controladores/usuarios');
 const verificarLogin = require('./intermediarios/verificarLogin');
 const { listarCategorias } = require('./controladores/categorias');
-const { listarTransacoes } = require('./controladores/transacoes');
+const { listarTransacoes, encontrarTransacaoPorId, editarTransacao } = require('./controladores/transacoes');
 
 const rotas = express();
 
@@ -11,8 +11,22 @@ rotas.post('/login', login);
 
 rotas.use(verificarLogin);
 
-rotas.get('/usuario', detalharUsuario)
-rotas.get('/categoria', listarCategorias)
-rotas.get('/transacao', listarTransacoes)
+rotas.get('/usuario', detalharUsuario);
+rotas.get('/categoria', listarCategorias);
+
+rotas.get('/transacao', listarTransacoes);
+rotas.get('/transacao/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const transacao = await encontrarTransacaoPorId(id);
+    if (!transacao) {
+      return res.status(404).json({ mensagem: 'Transação não encontrada.' });
+    }
+    return res.status(200).json(transacao);
+  } catch (error) {
+    return res.status(500).json({ mensagem: 'Erro interno do servidor.' });
+  }
+});
+rotas.put('/transacao/:id', editarTransacao);
 
 module.exports = rotas;
